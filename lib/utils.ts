38efea86ce7ae51716917d4c6bc482c597ee43e7
djhighson <djhighson@gmail.com>
2025-11-24 22:47:17 -0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,13 @@
-export function cn(...classes: any[]) {
-  return classes.filter(Boolean).join(" ");
+export async function searchYouTube(query: string) {
+  const url = `https://yt-api.p.rapidapi.com/search?query=${encodeURIComponent(query)}`;
+
+  const response = await fetch(url, {
+    headers: {
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY!,
+      "X-RapidAPI-Host": "yt-api.p.rapidapi.com"
+    }
+  });
+
+  const data = await response.json();
+  return data;
 }
